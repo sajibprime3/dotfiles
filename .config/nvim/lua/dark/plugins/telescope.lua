89fdc9826774b4e7f2e946febab2c4e0633e3201
Sajib Ahmed <sajibprime3@gmail.com>
2025-07-04 22:04:42 +0600

    add telescope.

---
+++ .config/nvim/lua/dark/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  config = function()
+    require("dark.telescope")
+  end,
+}
---
+++ .config/nvim/lua/dark/telescope.lua
@@ -0,0 +1,28 @@
+require("telescope").setup({
+  defaults = {
+    file_ignore_petterns = { "dune.lock" },
+  },
+  extensions = {
+    wrap_results = true,
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case",
+    },
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown({}),
+    },
+  },
+})
+
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "ui-select")
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "(f)ind (f)iles." })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>f/", builtin.current_buffer_fuzzy_find)
