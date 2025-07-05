b4605d7a34d9d4fd450e05e62ab948be968f52f0
Sajib Ahmed <sajibprime3@gmail.com>
2025-07-05 12:58:04 +0600

    add oil.nvim.
    Edit Files like buffer or butter. :)

---
+++ .config/nvim/lua/dark/config/oil.lua
@@ -0,0 +1,12 @@
+require("oil").setup({
+  default_file_explorer = true,
+  columns = {
+    "icon",
+    -- "permission",
+    -- "size",
+    -- "mtime",
+  },
+  view_options = {
+    show_hidden = true,
+  },
+})
