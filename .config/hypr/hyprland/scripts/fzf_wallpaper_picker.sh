332ff4b09e1c5db174425d84a10bb378cd931bc4
Sajib Ahmed <sajibprime3@gmail.com>
2025-10-27 03:07:06 +0600

    More Matugen options.

--- .config/hypr/hyprland/scripts/fzf_wallpaper_picker.sh
+++ .config/hypr/hyprland/scripts/fzf_wallpaper_picker.sh
@@ -3,14 +3,62 @@
 ## Change to Your Wallpapers Directory.
 wallpapers_dir=~/Pictures/wallpapers
 
-selected_wallpaper_name=$(ls $wallpapers_dir | fzf --prompt="Select Wallpaper > ")
-# If user cancelled
-[[ -z "$selected_wallpaper_name" ]] && exit 0
+mode="none"
+type="none"
+wallpaper="none"
+menu="fzf"
 
-wallpaper="$wallpapers_dir/$selected_wallpaper_name"
+fzf_type_menu() {
+    slt=$(echo -e "scheme-content\nscheme-expressive\nscheme-fidelity\nscheme-fruit-salad\nscheme-monochrome\nscheme-neutral\nscheme-rainbow\nscheme-tonal-spot" | fzf)
+    [[ -z "$slt" ]] && menu="fzf" && return
+    type=$slt
+    menu="fzf"
+}
+fzf_mode_menu() {
+    slm=$(echo -e "dark\nlight" | fzf)
+    [[ -z "$slm" ]] && menu="fzf" && return
+    mode=$slm
+    menu="fzf"
+}
+fzf_menu() {
+    sl=$( (
+        echo -e "Change Mode\nChange Type"
+        ls $wallpapers_dir
+    ) | fzf --prompt="Select Wallpaper > ")
+    [[ -z "$sl" ]] && menu="none" && return
+    case "$sl" in
+    "Change Mode")
+        menu="mode"
+        ;;
+    "Change Type")
+        menu="type"
+        ;;
+    *)
+        wallpaper="$wallpapers_dir/$sl"
+        ;;
+    esac
+}
+while true; do
+    case "$menu" in
+    "fzf")
+        fzf_menu
+        ;;
+    "mode")
+        fzf_mode_menu
+        ;;
+    "type")
+        fzf_type_menu
+        ;;
+    "none")
+        break
+        ;;
+    *)
+        echo "Something went wrong!"
+        ;;
+    esac
+done
 
-matugen -m light image $wallpaper
-
+# FIX: crash if didn't select all three every instance.
+matugen -m $mode -t $type image $wallpaper
 # A delay is needed for Matugen to compile and use Hooks. You need increase [or decrease] this delay if needed.
 sleep 0.7
-
