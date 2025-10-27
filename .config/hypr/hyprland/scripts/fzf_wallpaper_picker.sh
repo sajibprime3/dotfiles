fa7918bd48b20a8df039f5c1758593843852279f
Sajib Ahmed <sajibprime3@gmail.com>
2025-10-28 02:23:00 +0600

    More better Wallpaper picker.

--- .config/hypr/hyprland/scripts/fzf_wallpaper_picker.sh
+++ .config/hypr/hyprland/scripts/fzf_wallpaper_picker.sh
@@ -15,24 +15,66 @@ else
     type="scheme-tonal-spot"
 fi
 
+main_options() {
+    echo "0. Change Mode"
+    echo "00. Change Type"
+    local i=1
+    for item in $(ls $wallpapers_dir); do
+        if [[ "$item" == "$(basename $wallpaper)" ]]; then
+            echo "$i. $item *"
+        else
+            echo "$i. $item"
+        fi
+        ((i = i + 1))
+    done
+}
+mode_options() {
+    options=("dark" "light")
+    local i=1
+    for option in ${options[@]}; do
+        if [[ "$option" == "$mode" ]]; then
+            echo "$i. $option *"
+        else
+            echo "$i. $option"
+        fi
+        ((i = i + 1))
+    done
+}
+type_options() {
+    options=("scheme-content" "scheme-expressive" "scheme-fidelity" "scheme-fruit-salad" "scheme-monochrome" "scheme-neutral" "scheme-rainbow" "scheme-tonal-spot")
+    local i=1
+    for option in ${options[@]}; do
+        if [[ "$option" == "$type" ]]; then
+            echo "$i. $option *"
+        else
+            echo "$i. $option"
+        fi
+        ((i = i + 1))
+    done
+}
+remove_mark() {
+    local marked="$1"
+    local unmarked=$(echo "$marked" | sed -e 's:^[0-9]*. ::' -e 's:[ -*]*$::')
+    echo "$unmarked"
+}
 fzf_type_menu() {
-    slt=$(echo -e "scheme-content\nscheme-expressive\nscheme-fidelity\nscheme-fruit-salad\nscheme-monochrome\nscheme-neutral\nscheme-rainbow\nscheme-tonal-spot" | fzf)
-    [[ -z "$slt" ]] && fzf_menu && return
+    slt=$(type_options | fzf)
+    [[ -z "$slt" ]] && fzf_main_menu && return
+    slt=$(remove_mark "$slt")
     type=$slt
-    fzf_menu
+    fzf_main_menu
 }
 fzf_mode_menu() {
-    slm=$(echo -e "dark\nlight" | fzf)
-    [[ -z "$slm" ]] && fzf_menu && return
+    slm=$(mode_options | fzf)
+    [[ -z "$slm" ]] && fzf_main_menu && return
+    slm=$(remove_mark "$slm")
     mode=$slm
-    fzf_menu
+    fzf_main_menu
 }
-fzf_menu() {
-    sl=$( (
-        echo -e "Change Mode\nChange Type"
-        ls $wallpapers_dir
-    ) | fzf --prompt="Select Wallpaper > ")
+fzf_main_menu() {
+    sl=$(main_options | fzf --prompt="Select Wallpaper > ")
     [[ -z "$sl" ]] && return
+    sl=$(remove_mark "$sl")
     case "$sl" in
     "Change Mode")
         fzf_mode_menu
@@ -42,12 +84,12 @@ fzf_menu() {
         ;;
     *)
         wallpaper="$wallpapers_dir/$sl"
-        fzf_menu
+        fzf_main_menu
         ;;
     esac
 }
 
-fzf_menu
+fzf_main_menu
 
 matugen -m $mode -t $type image $wallpaper
 
