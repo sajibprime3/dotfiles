2a410c313a5a47ca73967c963507b986dbbd2f9a
Sajib Ahmed <sajibprime3@gmail.com>
2025-12-31 01:45:50 +0600

    Add more colorschemes.

--- .config/nvim/lua/dark/plugins/colorschemes.lua
+++ .config/nvim/lua/dark/plugins/colorschemes.lua
@@ -1,5 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "moon" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  { "rose-pine/neovim" },
+  "rebelot/kanagawa.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
